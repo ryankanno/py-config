04adc5564566bee2a27fbedc5f6a678ecfc6e72f
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-03-22 09:05:48 -0400

    Added initial commit of locator

---
+++ Makefile
@@ -0,0 +1,19 @@
+NOSETESTS ?= nosetests
+
+.PHONY: nosetests nosetests.coverage test test.coverage flake8
+
+test: nosetests flake8
+test.coverage: nosetests.coverage flake8
+
+nosetests:
+	@$(NOSETESTS) --with-doctest
+
+nosetests.coverage:
+	@$(NOSETESTS) --with-xcoverage --cover-package=py_config --cover-tests --with-doctest
+
+flake8:
+	@flake8 py_config tests
+
+clean:
+	@rm -rf .coverage
+	@rm -rf coverage.xml
--- py_config/config_locator.py
+++ py_config/config_locator.py
@@ -6,23 +6,29 @@ import os
 
 logger = logging.getLogger(__name__)
 
-PY_CONFIG_PATH = "PY_CONFIG_FILE"
+PY_CONFIG_KEY = "PY_CONFIG_FILE"
 
 
 class ConfigLocator(object):
-    def __init__(self, cwd, config_name):
-        self.cwd = cwd
+    def __init__(self, config_name, local_dir='./', env_key=PY_CONFIG_KEY):
         self.config_name = config_name
+        self.env_key = env_key
+        self.local_dir = local_dir
 
-    def get_config(self):
-        pass
+    def get_config_search_paths(self):
+        return [os.path.join(path, self.config_name)
+                for path in self._get_search_paths]
 
-    def get_search_paths(self):
-        paths = ["{0}/{1}".format(self._get_home_dir(), self.config_name)]
-        return paths
+    def _get_search_paths(self):
+        return [
+            self._get_env_dir(),
+            self._get_local_dir(),
+            self._get_home_dir(),
+            self._get_system_dir()
+        ]
 
-    def _get_curr_dir(self):
-        return self.cwd
+    def _get_local_dir(self):
+        return self.local_dir
 
     def _get_home_dir(self):
         return os.path.expanduser("~")
@@ -31,6 +37,6 @@ class ConfigLocator(object):
         return ""
 
     def _get_env_dir(self):
-        return os.environ.get(PY_CONFIG_PATH, None)
+        return os.environ.get(self.env_key, None)
 
 # vim: filetype=python
