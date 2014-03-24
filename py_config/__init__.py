4cc0b5531f353c7da650b328410a85caabe2468a
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-03-24 07:34:24 -0400

    Added fixed __init__.py Cleaned up config

--- py_config/__init__.py
+++ py_config/__init__.py
@@ -1,1 +1,3 @@
 from config_locator import ConfigLocator
+
+__all__ = [ConfigLocator]
---
+++ py_config/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Config(object):
+    pass
+
+
+# vim: filetype=python
