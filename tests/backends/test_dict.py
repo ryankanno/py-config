50d00abf128ba4c63a7edcb8cefbcc28a579749c
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-25 08:24:30 -0500

    Added DictionaryProviderBackend test

---
+++ tests/backends/test_dict.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import eq_
+from py_config.backends.dict import DictionaryProviderBackend
+import unittest
+
+
+class TestDictionaryProviderBackend(unittest.TestCase):
+
+    def test_dict(self):
+        dict = {'foo': 'bar', 'too': 'much'}
+        provider = DictionaryProviderBackend(dict)
+        provider.set('python', 'rules')
+        eq_('bar', provider.get('foo', None))
+        eq_(None, provider.get('foo2', None))
+        eq_(1, provider.get('foo3', 1))
+        eq_('rules', provider.get('python', None))
+
+
+# vim: filetype=python
