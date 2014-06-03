67626a7302fed46f0a729fc4d742bf6bcffae43d
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-03 06:40:54 -0400

    Fixed up for pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import re
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
-import py_config
 
 packages = [
     'py_config',
     'py_config.backends',
 ]
 
+here = os.path.dirname(os.path.realpath(__file__))
+
+# Metadata
+
+meta = {}
+re_meta = re.compile(r'__(\w+?)__\s*=\s*(.*)')
+re_version = re.compile(r'VERSION\s*=.*?\((.*?)\)')
+strip_quotes = lambda s: s.strip("\"'")
+
+
+def add_version(match):
+    return {'VERSION': match.group(1).replace(" ", "").replace(",", ".")}
+
+
+def add_meta(match):
+    attr_name, attr_value = m.groups()
+    return {attr_name: strip_quotes(attr_value)}
+
+
+patterns = {
+    re_meta: add_meta,
+    re_version: add_version
+}
+
+
+with open(os.path.join(here, 'py_config/__init__.py'), 'r') as f:
+    for line in f:
+        for pattern, handler in patterns.items():
+            m = pattern.match(line.strip())
+            if m:
+                meta.update(handler(m))
+
+# Requires
+
 requires = ['pyyaml']
 tests_require = ['flake8', 'mock', 'nose', 'nosexcover']
 
-with open('README.rst') as f:
+with open(os.path.join(here, 'README.rst')) as f:
     readme = f.read()
 
-with open('CHANGES') as f:
+with open(os.path.join(here, 'CHANGES')) as f:
     changes = f.read()
 
 classifiers = [
@@ -32,17 +67,17 @@ classifiers = [
 
 setup(
     name='py-config',
-    version=py_config.__version__,
+    version=meta['VERSION'],
     description='Tiny configuration wrapper module',
     long_description=readme + '\n\n' + changes,
-    author='Ryan Kanno',
-    author_email='ryankanno@localkinegrinds.com',
+    author=meta['author'],
+    author_email=meta['email'],
     url="https://github.com/ryankanno/py-config",
     packages=packages,
     package_data={'': ['LICENSE']},
     package_dir={'py_config': 'py_config'},
     install_requires=requires,
-    license='MIT',
+    license=meta['license'],
     tests_require=tests_require,
     classifiers=classifiers,
 )
