46ab895b8573f276010436af517b8c3416f7f4c5
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-17 08:16:46 -0400

    Added provider test

---
+++ py_config/backends/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ini import IniProviderBackend
+import os
+
+
+def get_provider(path):
+    file_name, file_ext = os.path.splitext(path)
+    return {
+        '.ini': IniProviderBackend
+    }.get(file_ext.lower())(path)
+
+# vim: filetype=python
---
+++ tests/backends/test_provider.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import ok_
+import os
+from py_config.backends import get_provider
+from py_config.backends.ini import IniProviderBackend
+import unittest
+
+
+class TestProviderBackend(unittest.TestCase):
+
+    def setUp(self):
+        self.cwd = os.path.dirname(os.path.realpath(__file__))
+        self.ini_file = os.path.join(self.cwd, '.', 'data', 'foobar.ini')
+
+    def test_get_provider(self):
+        provider = get_provider(self.ini_file)
+        ok_(type(provider) == IniProviderBackend)
+
+
+# vim: filetype=python
