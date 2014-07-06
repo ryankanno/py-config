f671f04a6d5af85d8ca212a05163f8ba38829c5f
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-07-06 08:53:34 -0400

    Added more yaml tests

--- tests/backends/test_yaml.py
+++ tests/backends/test_yaml.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from nose.tools import eq_
+from nose.tools import raises
 import os
+from py_config.backends.base import ConfigException
 from py_config.backends.yaml import YamlProviderBackend
 import tempfile
 import unittest
 
 
-class TestIniProviderBackend(unittest.TestCase):
+class TestYamlProviderBackend(unittest.TestCase):
 
     def setUp(self):
         self.cwd = os.path.dirname(os.path.realpath(__file__))
@@ -24,5 +26,24 @@ class TestIniProviderBackend(unittest.TestCase):
         eq_(None, provider.get('Foo.Bar', None))
         provider.set('Foo.Bar', '2')
         eq_('2', provider.get('Foo.Bar', None))
+
+        # New
+        provider.set('Foo', 'Ball')
+        eq_('Ball', provider.get('Foo', None))
+
+    @raises(ConfigException)
+    def test_yml_invalid_set_should_raise_exception(self):
+        provider = YamlProviderBackend(self.yml_file)
+        provider.set(None, None)
+
+    @raises(ConfigException)
+    def test_yml_invalid_delete_should_raise_exception(self):
+        provider = YamlProviderBackend(self.yml_file)
+        provider.delete(None)
+
+    @raises(ConfigException)
+    def test_invalid_yml_location_should_raise_exception(self):
+        YamlProviderBackend('/foo/bar')
+
 
 # vim: filetype=python
