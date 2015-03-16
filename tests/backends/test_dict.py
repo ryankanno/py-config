d90c33ee6209084344ae8944f4c0f3a606bd4e12
Ryan Kanno <ryankanno@localkinegrinds.com>
2015-03-16 12:42:07 -1000

    Added tests

--- tests/backends/test_dict.py
+++ tests/backends/test_dict.py
@@ -8,16 +8,28 @@ import unittest
 
 class TestDictionaryProviderBackend(unittest.TestCase):
 
-    def test_dict(self):
-        dict = {'foo': 'bar', 'too': 'much'}
-        provider = DictionaryProviderBackend(dict)
-        provider.set('python', 'rules')
-        eq_('bar', provider.get('foo', None))
-        eq_(None, provider.get('foo2', None))
-        eq_(1, provider.get('foo3', 1))
-        eq_('rules', provider.get('python', None))
-        provider.delete('python')
-        eq_(-1, provider.get('python', -1))
+    def setUp(self):
+        self.dict = {'foo': 'bar', 'too': 'much'}
+        self.provider = DictionaryProviderBackend(self.dict)
+
+    def test_dict_get_value_that_exists(self):
+        eq_('bar', self.provider.get('foo', None))
+
+    def test_dict_get_defaults(self):
+        eq_(None, self.provider.get('foo2', None))
+        eq_(1, self.provider.get('foo3', 1))
+
+    def test_dict_set(self):
+        self.provider.set('python', 'rules')
+        eq_('rules', self.provider.get('python', None))
+
+    def test_dict_delete(self):
+        self.provider.delete('python')
+        eq_(-1, self.provider.get('python', -1))
+        eq_(None, self.provider.get('python', None))
+
+    def test_dict_to_dict(self):
+        eq_(self.dict, self.provider.to_dict())
 
 
 # vim: filetype=python
